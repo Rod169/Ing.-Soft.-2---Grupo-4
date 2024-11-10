f516b0fa8889474d4aa0476a086abd0e021b3e82
FZuasnabar <139509842+FZuasnabar@users.noreply.github.com>
2024-11-10 17:40:50 -0500

    Header actualizado
    Error de header desactulizado al iniciar sesión solucionado.
    Muestra las secciones que son accesible de acuerdo al tipo de cuenta.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   const [menuDesplegable, setMenuDesplegable] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState(null); // Estado para almacenar el nombre del usuario autenticado
-  const [userType, setUserType] = useState(null); // Estado para manejar el tipo de usuario (empresa o proveedor)
+  const [userType, setUserType] = useState(null); // Estado para manejar el tipo de usuario (empresa, proveedor o cliente)
   const navigate = useNavigate();
+  const location = useLocation(); // Hook para obtener la ubicación actual
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem('loggedInUser')); // Obtiene el usuario almacenado en localStorage
     if (user) {
       setLoggedInUser(user.firstName); // Establece el firstName del usuario en el estado
-      setUserType(user.accountType); // Establece el tipo de cuenta (proveedor o empresa)
+      setUserType(user.accountType); // Establece el tipo de cuenta (proveedor, empresa o cliente)
     }
-  }, []);
+  }, [location]); // La dependencia 'location' asegura que se actualice al cambiar la ruta
 
   const alternarMenuDesplegable = () => {
     setMenuDesplegable(!menuDesplegable); // Cambia el estado de visibilidad del menú
@@ -34,12 +35,18 @@ const Header = () => {
       </div>
       <nav className="nav-links">
         <a href="/inicio">Inicio</a>
-        {userType === 'empresa' ? (
-          <a href="/pedidos-empresa">Pedidos Empresa</a>
-        ) : (
+        {userType === 'proveedor' && (
           <a href="/pedidos-proveedor">Pedidos Proveedor</a>
         )}
-        <a href="#">Productos</a>
+        {userType === 'empresa' && (
+          <>
+            <a href="/pedidos-empresa">Pedidos Empresa</a>
+            <a href="/productos-empresa">Productos Empresa</a>
+          </>
+        )}
+        {userType === 'cliente' && (
+          <a href="/productos-cliente">Productos Cliente</a>
+        )}
         <a href="#">Sobre nosotros</a>
         <div className="support-dropdown">
           <a href="/soporte">Soporte &#9662;</a>
