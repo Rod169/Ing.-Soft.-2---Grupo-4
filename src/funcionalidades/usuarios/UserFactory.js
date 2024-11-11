e52d3d12093b2f6a024ac4415a6fc8cc4470c325
FZuasnabar <139509842+FZuasnabar@users.noreply.github.com>
2024-11-10 19:33:56 -0500

    Actualización general

--- src/funcionalidades/usuarios/UserFactory.js
+++ src/funcionalidades/usuarios/UserFactory.js
@@ -1,41 +1,48 @@
 // funcionalidades/usuarios/UserFactory.js
 
-
 class User {
-    constructor(firstName, lastName, email, password) {
+  constructor(firstName, lastName, email, password) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
       this.password = password;
-    }
   }
-  
-  class Proveedor extends User {
-    constructor(firstName, lastName, email, password) {
+}
+
+class Proveedor extends User {
+  constructor(firstName, lastName, email, password) {
       super(firstName, lastName, email, password);
       this.accountType = 'proveedor';
-    }
   }
-  
-  class Empresa extends User {
-    constructor(firstName, lastName, email, password) {
+}
+
+class Empresa extends User {
+  constructor(firstName, lastName, email, password) {
       super(firstName, lastName, email, password);
       this.accountType = 'empresa';
-    }
   }
-  
-  class UserFactory {
-    static createUser(accountType, firstName, lastName, email, password) {
+}
+
+class Cliente extends User {
+  constructor(firstName, lastName, email, password) {
+      super(firstName, lastName, email, password);
+      this.accountType = 'cliente';
+  }
+}
+
+class UserFactory {
+  static createUser(accountType, firstName, lastName, email, password) {
       switch (accountType) {
-        case 'proveedor':
-          return new Proveedor(firstName, lastName, email, password);
-        case 'empresa':
-          return new Empresa(firstName, lastName, email, password);
-        default:
-          throw new Error('Tipo de cuenta no válido');
+          case 'proveedor':
+              return new Proveedor(firstName, lastName, email, password);
+          case 'empresa':
+              return new Empresa(firstName, lastName, email, password);
+          case 'cliente':
+              return new Cliente(firstName, lastName, email, password);
+          default:
+              throw new Error('Tipo de cuenta no válido');
       }
-    }
   }
-  
-  export default UserFactory;
-  
+}
+
+export default UserFactory;
